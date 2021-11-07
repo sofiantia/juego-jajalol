27e6cbe51f39c2a5c3d66547ad3193c6831d23f6
Sofia Restrepo <sofia.restrepov@upb.edu.co>
2021-11-07 16:17:40 -0500

    Final del juego

--- js/main.js
+++ js/main.js
@@ -1,33 +1,76 @@
+var tiro_1;
+var tiro_2;
 var posiciones = [0,-160,-320,-481,-642,-803];
 var dado1,dado2,boton_tirar;
+var suma;
+var turno = 0;
+var punto = 0;
 
 window.onload = init;
 
-function init(){
-  dado1 = document.getElementById("dado1");
-  dado2 = document.getElementById("dado2");
+
+function init (){
   boton_tirar = document.getElementById("boton_tirar");
   boton_tirar.addEventListener("click",jugar);
+
+  dado1 = document.getElementById("dado1");
+  dado2 = document.getElementById("dado2");
 }
 
-//https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-function tirardado(ref){
-  var cara = Math.floor(Math.random() * 6) + 1 ;
+function tirardado(){
+  return Math.floor(Math.random() * 6) + 1 ;
+}
+
+function actualizarDado(ref,cara){
   ref.style.backgroundPosition = posiciones[cara-1]+"px";
-return cara;
 }
 
 function jugar(){
-  let tiro1 = tirardado(dado1);
-  let tiro2 = tirardado(dado2);
-  let suma = tiro1 + tiro2;
+  tiro_1 = tirardado();//Retorna un numero entre 1 y 6
+  tiro_2 = tirardado();  
+  actualizarDado(dado1,tiro_1);
+  actualizarDado(dado2,tiro_2);
 
-  console.log ("Tiro 1:"+tiro1)
-console.log ("Tiro 2:"+tiro2)
+  turno = turno + 1;
+  console.log("Turno "+ turno );
+  suma = tiro_1 + tiro_2
 
+    if (turno == 1)
+    { 
+      if (suma == 7 || suma == 11)
+      {
+        alert("¡Ganaste la partida! (ﾉ>ω<)ﾉ :｡･:*:･ﾟ’★,｡･:*:･ﾟ’☆");
+        turno = 0; 
+        punto = 0;
+      }
 
-if (suma ==7 || suma ==11) 
-{
-alert("Ganaste la partida")
-}
+      if (suma == 2 || suma == 3 || suma == 12)
+      {
+        alert("¡La casa ha ganado la partida! ╮(︶︿︶)╭");
+        turno = 0;
+        punto = 0;
+      }
+
+      if (suma == 4 || suma == 5 || suma == 6 || suma == 8 || suma == 9 || suma == 10)
+      {
+        alert("Obtuviste (≧▽≦)/ "+ suma );
+        punto = suma;
+      }
+    }
+
+    if (turno > 1)
+    {
+      if (punto == suma)
+      {
+        alert("¡Ganaste la partida!(^◕ᴥ◕^)");
+        turno = 0;
+        punto = 0;
+      }
+      if (suma == 7)
+      {
+        alert("¡Has perdido!｡･ﾟﾟ*(>д<)*ﾟﾟ･｡");
+        turno = 0;
+        punto = 0;
+      }
+    } 
 }
